fb80048d5d8fd3a98a939d94f41f914b8b1aea8b
Alejandro Quiros <alejandro.quiros.alfaro@ucreativa.com>
2021-08-07 09:18:26 -0600

    Include IE and switch for multi browsing

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
@@ -22,14 +24,24 @@ public class BaseClass {
     @BeforeMethod
     public void BeforeMethod(@Optional("chrome") String browser){
       //  System.out.println("** Esto corre dos vez");
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
 
-        if (browser.equals("firefox"))
-            driver = new FirefoxDriver();
-        else
-            driver = new ChromeDriver();
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                InternetExplorerOptions options = new InternetExplorerOptions();
+                options.ignoreZoomSettings();
 
+                driver = new InternetExplorerDriver(options);
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
         driver.get("https://demo.opencart.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
